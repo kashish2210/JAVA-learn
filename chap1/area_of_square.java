7e21c2791e4f4eaa3cb0c8809a1866e1174452a7
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-12-11 18:35:46 +0530

    Create area_of_square.java

---
+++ chap1/area_of_square.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class area_of_square{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float r = sc.nextFloat();
+        System.out.println("area of square" + (r*r));
+        sc.close();
+    }
+}
