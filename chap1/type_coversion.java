ff4ca9334f0ec34aeda06c09696597612cc76d3c
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-12-11 18:50:25 +0530

    Create type_coversion.java

---
+++ chap1/type_coversion.java
@@ -0,0 +1,12 @@
+
+public class type_coversion{
+    public static void main(String[] args) {
+        // int a = 25;
+        // long b = a;{will work fine}
+        // long a = 25;
+        // int b = a; // will give error
+        // byte --> short[2 bytes] --> int[4 bytes] --> long[8 bytes] --> float[16 bytes] --> double[32 bytes]
+        System.out.println(b);
+    }
+ 
+}
