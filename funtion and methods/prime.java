e40ca4ba187ff8540aa7ba10cf1c22c0e7716c06
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-12-15 18:27:34 +0530

    Create prime.java

---
+++ funtion and methods/prime.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class prime{
+    public static boolean isPrime(int n){
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println("enter the no to check whether it is a prime or not");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+        sc.close();
+    }
+}
