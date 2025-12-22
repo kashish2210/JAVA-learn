f2b92264d637741315f999dae40b749e84b540bf
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-12-23 04:06:24 +0530

    rhombus pattern

---
+++ patterns/rohmbus.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class rohmbus{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        for(int i =1; i <= size ; i++){
+            for(int j = 1 ; j <=(size-i); j++) {
+                System.out.print(" ");
+            }
+            for(int j =1 ; j <= size ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
