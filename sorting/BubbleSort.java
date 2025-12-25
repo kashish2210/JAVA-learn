0eac308b31faefbe792fb719ec00065c069e31ad
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-12-26 03:22:24 +0530

    bubbleSort

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
