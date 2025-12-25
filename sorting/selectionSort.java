fdd1634338ca402b4085dc472497d41d9376c627
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-12-26 04:55:55 +0530

    selection sort

---
+++ sorting/selectionSort.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class selectionSort {
+
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i+1; j < arr.length ; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos=j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
