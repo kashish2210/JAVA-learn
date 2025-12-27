486f7b71d302f734b692836bb9feec246d90cdd3
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-12-28 05:00:59 +0530

    advance spiral

--- 2darray/spiral.java
+++ 2darray/spiral.java
@@ -1,34 +1,39 @@
 import java.util.*;
-public class spiral
-{   public static void printSpiral(int matrix[][]){
-    int startRow = 0 ;
-    int startCol = 0 ;
-    int endRow = matrix.length - 1;
-    int endCol = matrix[0].length - 1;
-    while(startRow <= endRow && startCol <= endCol){
-        //top
-        for(int j = startCol ; j <= endCol ; j++){
-            System.out.println(matrix[startRow][j]+" ");
+public class spiral{
+    public static void printSpiral(int matrix[][]){
+        int startRow = 0 ;
+        int startCol = 0 ;
+        int endRow = matrix.length - 1;
+        int endCol = matrix[0].length - 1;
+        while(startRow <= endRow && startCol <= endCol){
+            //top
+            for(int i = startCol ; i <=endCol ; i++){
+                System.out.print(matrix[startRow][i]+" ");
+            }
+            //bottom
+            for(int i = startRow + 1; i<=endRow ; i++){
+                System.out.print(matrix[i][endCol]+" ");
+            }
+            //right
+            if(startRow < endRow){
+                for(int j = endCol - 1; j >= startCol; j--){
+                    System.out.print(matrix[endRow][j] + " ");
+                }
+            }
+
+            // LEFT
+            if(startCol < endCol){
+                for(int i = endRow - 1; i > startRow; i--){
+                    System.out.print(matrix[i][startCol] + " ");
+                }
+            }
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
         }
-        //right
-        for(int i = startRow+1; i <= endRow ; i++){
-            System.out.println(matrix[i][endCol]+" ");
-        }
-        //bottom
-        for(int k = endCol-1; k >= startCol ; k--  ){
-            System.out.println(matrix[endRow][k]+" ");
-        }
-        //left
-        for(int i = endRow -1 ; i >=startRow ; i--){
-            System.out.println(matrix[i][startCol]+" ");
-        }
-        startCol++;
-        startRow++;
-        endCol--;
-        endRow--;
+        System.out.println();
     }
-    System.out.println();
-}
     public static void main(String args[]){
         int[][] matrix = new int[4][4];
         int n = matrix.length ,m= matrix[0].length;
